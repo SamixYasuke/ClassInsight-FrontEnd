e490afad980ac0d6af0caf7e699de748338a59f2
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-01-28 19:40:47 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-nnyb77pycgq3l6h1.us.auth0.com"
-      clientId="rsgxF8HBiYXUwyqomz3HIR56oOAaq17K"
+      clientId="9k0iZK63Ol2vMotN4myLv4AuPMAoNrUl"
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
