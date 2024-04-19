4320ce73e64f40f986bdc96266fc417bfbcde82f
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-04-19 15:17:51 +0100

    Update PieChart.jsx

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,21 +1,52 @@
 import React from "react";
-import { Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 
 const PieChart = ({ chartData, aspect }) => {
   const options = {
     plugins: {
       legend: {
-        display: false, // Set to true if you want to display the legend
+        display: true,
+        position: "top",
+        labels: {
+          boxWidth: 8, // Adjust the width of each legend item as needed
+          usePointStyle: true, // Use point style for the legend items (optional)
+        },
       },
     },
   };
 
   return (
     <>
-      <Pie data={chartData} options={options} />
-      <p>{aspect}</p>
+      <div>
+        <Doughnut data={chartData} options={options} />
+        <p style={{ textAlign: "center" }}>{aspect}</p>
+      </div>
     </>
   );
 };
 
 export default PieChart;
+
+// import React from "react";
+// import { Pie, Doughnut, Bubble } from "react-chartjs-2";
+
+// const PieChart = ({ chartData, aspect }) => {
+//   const options = {
+//     plugins: {
+//       legend: {
+//         display: false, // Set to true if you want to display the legend
+//       },
+//     },
+//   };
+
+//   return (
+//     <>
+//       <Doughnut data={chartData} options={options} />
+//       <p>{aspect}</p>
+//       {/* <Pie data={chartData} options={options} />
+//       <p>{aspect}</p> */}
+//     </>
+//   );
+// };
+
+// export default PieChart;
